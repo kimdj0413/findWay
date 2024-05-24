c2e73bd9f47357daf1717b4685e3c89192dd5e99
KimDJ <ejuniya@naver.com>
2024-05-24 13:57:55 +0900

    202405241357

--- Q_Learning/DQN_4.py
+++ Q_Learning/DQN_4.py
@@ -6,18 +6,19 @@ import torch.nn as nn
 import torch.optim as optim
 import time
 import datetime
-import os
+import torch.nn.functional as F
 
 start_time = time.time()
 class GridSetting:
     def __init__(self):
         self.grid_size_x = 10
         self.grid_size_y = 10
-        self.avoid = [(0,3),(1,1),(1,5),(1,8),(2,1),(2,3),(2,5),(3,0),(3,7),(4,4),(4,7),(4,9),(5,1),(5,3),(6,6),(6,9),(7,2),(7,4),(7,7),(8,1),(8,3),(9,2),(9,6)]
+        self.avoid = [(0,5),(0,6),(0,7),(0,8),(0,9),(1,0),(1,1),(1,2),(1,5),(1,6),(1,7),(1,8),(1,9),(2,0),(2,1),(2,2),(2,5),(2,6),(2,7),(2,8),(2,9),(3,0),(3,1),(3,2),(3,5),(3,6),(3,7),(3,8),(3,9),(4,9),(5,4),(5,5),(5,6),(5,7),(5,9),(6,0),(6,1),(6,2),(6,4),(6,5),(6,6),(6,7),(7,0),(7,1),(7,2),(8,0),(8,1),(8,2),(8,5),(8,6),(8,7),(9,5),(9,6),(9,7)]
         # self.grid_size_x = 5
         # self.grid_size_y = 5
-        # self.avoid = [(0,1),(1,1),(1,3),(2,3),(3,0),(3,1),(3,2),(3,3),(3,4),(4,1),(4,2)]
+        # self.avoid = [(0,1),(1,1),(1,3),(2,3),(3,0),(3,1),(3,2),(3,3),(4,1),(4,2)]
         self.start_state = (0,0)
+        self.force_togo = 3
         self.end_state = (self.grid_size_x-1,self.grid_size_y-1)
 
     def reset(self):
@@ -44,7 +45,7 @@ class GridSetting:
             reward +=  1
             done = True
         else:
-            reward += -0.01
+            reward += -0.001
             done = False
         
         return next_state, reward, done
@@ -73,16 +74,17 @@ class ReplayBuffer:
 class DQNAgent:
     def __init__(self, input_size, output_size, hidden_size=64, gamma=0.99):
         self.gamma = gamma
-        self.replay_buffer = ReplayBuffer(10000)
+        self.replay_buffer = ReplayBuffer(3000)
         self.model = QNetwork(input_size, output_size)
-        self.optimizer = optim.Adam(self.model.parameters())
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
 
     def update_model(self,batch_size):
         if len(self.replay_buffer) < batch_size:
             return
         batch = self.replay_buffer.sample(batch_size)
         states, actions, rewards, next_states, dones = zip(*batch)
-
+        if actions is None:
+            print(states,actions,rewards,next_states,dones)
         states = torch.FloatTensor(states)
         actions = torch.LongTensor(actions)
         rewards = torch.FloatTensor(rewards)
@@ -93,20 +95,29 @@ class DQNAgent:
         next_q_values = self.model(next_states).max(1)[0]
         expected_q_values = rewards + self.gamma * next_q_values * (1 - dones)
 
-        loss = nn.MSELoss()(current_q_values, expected_q_values.detach())
+        loss = nn.SmoothL1Loss()(current_q_values, expected_q_values.detach())
+
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
         
         return loss.item()
     
-    def select_action(self, state, epsilon):
+    def select_action(self, state, epsilon, cnt):
         state = torch.FloatTensor(state).unsqueeze(0)
         if random.random() < epsilon:
             return random.randint(0, 3)
         else :
             q_values = self.model(state)
-            return np.argmax(q_values.detach().numpy())
+            action = np.argsort(q_values.detach().numpy().squeeze())[::1]
+            if cnt == 0:
+                return action[0]
+            if cnt == 1:
+                return action[1]
+            if cnt == 2:
+                return action[2]
+            if cnt == 3:
+                return action[3]
         
     def sort_action(self, state):
         state = torch.FloatTensor(state).unsqueeze(0)
@@ -117,22 +128,33 @@ def train(agent, env, num_episodes, batch_size):
     with open('training_log.txt', 'w') as file:
         for episode in range(num_episodes):
             state = env.reset()
-            epsilon = 1.0-episode/num_episodes
-            # reward = 0
+            visited=set()
+            epsilon = max(0.1, 1.0 - episode / num_episodes)#1.0-episode/num_episodes
             done = False
             state_list=[]
             reward = 0.0
+            visited.add(state)
+            cnt = 0
 
             while not done:
-                action = agent.select_action(state,epsilon)
-                next_state, reward, done = env.move(state, action, reward)
+                while(True):
+                    action = agent.select_action(state,epsilon,cnt)
+                    next_state, reward, done = env.move(state, action, reward)
+                    if next_state not in visited:
+                        visited.add(next_state)
+                        break
+                    cnt += 1
+                    if cnt > 5:
+                        action = env.force_togo
+                        break
+                cnt = 0
                 agent.replay_buffer.push(state, action, reward, next_state, done)
                 loss = agent.update_model(batch_size)
                 state = next_state
                 state_list.append(next_state)
 
-            file.write(f'에피소드: {episode}, state: {state}, 보상: {reward}, Loss: {loss}\n')
-            print(f'에피소드: {episode}, state: {state}, 보상: {reward}, Loss: {loss}')
+            file.write(f'에피소드: {episode}, state: {state}, 보상: {reward*10:.2f}, Loss: {loss}\n')
+            print(f'에피소드: {episode}, state: {state}, 보상: {reward*10:.2f}, Loss: {loss}')
            
 def save_model(model, file_name):
     torch.save(model.state_dict(), file_name)
@@ -145,7 +167,7 @@ input_size = 2
 output_size = 4
 hidden_size = 128
 batch_size = 64
-num_episodes = 5000000
+num_episodes = 10000
 
 env = GridSetting()
 agent = DQNAgent(input_size, output_size, hidden_size)
@@ -165,7 +187,8 @@ save_model(agent.model, 'dqn_test_model.pth')
 #     while state != env.end_state:
 #         action = agent.sort_action(state)
 #         for i in action:
-#             next_state, _, done = env.move(state, i)
+#             next_state, _, done = env.move(state, i,0)
+#             # print(visited)
 #             if next_state in visited:
 #                 continue
 #             else:
@@ -186,7 +209,7 @@ save_model(agent.model, 'dqn_test_model.pth')
 
 # env = GridSetting()
 # new_agent = DQNAgent(input_size, output_size, hidden_size)
-# load_model(new_agent.model, 'dqn_model.pth')
+# load_model(new_agent.model, 'dqn_test_model.pth')
 
 # optimal_path, action_path= find_optimal_path(env, new_agent, env.start_state)
 # print(f'최적의 경로: {optimal_path,action_path}')
