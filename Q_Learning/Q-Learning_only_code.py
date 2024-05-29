239f81a055b122415e4f3acf5ddaa21e0d28b000
KimDJ <ejuniya@naver.com>
2024-05-29 18:28:14 +0900

    20240529

--- Q_Learning/Q-Learning_only_code.py
+++ Q_Learning/Q-Learning_only_code.py
@@ -7,38 +7,20 @@ from PIL import Image
 from numba import jit, njit, int32, float32
 
 start = time.time()
-def image_to_grid(image_path, grid_size=(480, 250)):
-    image = Image.open(image_path).convert('L')
-    image = image.resize(grid_size)
-    
-    image_array = np.array(image)
-    
-    threshold = 200
-    grid = (image_array < threshold).astype(int)
 
-    print(grid)
-
-    return grid
-
-grid = image_to_grid('map.jpg')
-must_avoid=[]
-for i in range(0,250):
-    for j in range(0,480):
-        if grid[i][j] == 1 :
-            must_avoid.append((i,j))
-grid_size_x = 250
-grid_size_y = 480
+grid_size_x = 10
+grid_size_y = 10
 
 # Q-테이블 초기화
 Q = np.zeros((grid_size_x, grid_size_y, 4))
 # alpha = 0.5
 gamma = 0.9  
 # epsilon = 0.7
-episodes = 100000
+episodes = 1000
 
-start_point = (2, 12)
-goal_point = (244,473)
-# must_avoid = [(0,5),(0,6),(0,7),(0,8),(0,9),(1,0),(1,1),(1,2),(1,5),(1,6),(1,7),(1,8),(1,9),(2,0),(2,1),(2,2),(2,5),(2,6),(2,7),(2,8),(2,9),(3,0),(3,1),(3,2),(3,5),(3,6),(3,7),(3,8),(3,9),(4,9),(5,4),(5,5),(5,6),(5,7),(5,9),(6,0),(6,1),(6,2),(6,4),(6,5),(6,6),(6,7),(7,0),(7,1),(7,2),(8,0),(8,1),(8,2),(8,5),(8,6),(8,7),(9,5),(9,6),(9,7)]
+start_point = (0, 0)
+goal_point = (9,9)
+must_avoid = [(0,5),(0,6),(0,7),(0,8),(0,9),(1,0),(1,1),(1,2),(1,5),(1,6),(1,7),(1,8),(1,9),(2,0),(2,1),(2,2),(2,5),(2,6),(2,7),(2,8),(2,9),(3,0),(3,1),(3,2),(3,5),(3,6),(3,7),(3,8),(3,9),(4,9),(5,4),(5,5),(5,6),(5,7),(5,9),(6,0),(6,1),(6,2),(6,4),(6,5),(6,6),(6,7),(7,0),(7,1),(7,2),(8,0),(8,1),(8,2),(8,5),(8,6),(8,7),(9,5),(9,6),(9,7)]
 
 # random_cnt=5000
 # while len(must_avoid) < random_cnt:
@@ -102,64 +84,64 @@ times = str(datetime.timedelta(seconds=sec))
 short = times.split(".")[0]
 print(f"{short} sec")
 
-# # 최적 경로 찾기
-# Q = np.load('q_table.npy')
-# state = (2,15)
-# visited_states = set()
-# visited_states.add(state)
-# optimal_path = [state]
-# while state != goal_point:
-#     sorted_actions = np.argsort(Q[state[0], state[1]])[::-1]
+# 최적 경로 찾기
+Q = np.load('q_table.npy')
+state = (0,0)
+visited_states = set()
+visited_states.add(state)
+optimal_path = [state]
+while state != goal_point:
+    sorted_actions = np.argsort(Q[state[0], state[1]])[::-1]
 
-#     for action_index in sorted_actions:
-#         action = actions[action_index]
-#         next_s = next_state(state, action)
-#         if next_s not in visited_states:
-#             state = next_s
-#             optimal_path.append(state)
-#             visited_states.add(state)
-#             break
-#     else:
-#         break
-#     print(f"State: {state}, Action: {action}")
-# print(f"최적 경로 : {optimal_path}")
+    for action_index in sorted_actions:
+        action = actions[action_index]
+        next_s = next_state(state, action)
+        if next_s not in visited_states:
+            state = next_s
+            optimal_path.append(state)
+            visited_states.add(state)
+            break
+    else:
+        break
+    print(f"State: {state}, Action: {action}")
+print(f"최적 경로 : {optimal_path}")
 
-# # 그리드 생성
-# fig, ax = plt.subplots(figsize=(20, 20))
+# 그리드 생성
+fig, ax = plt.subplots(figsize=(20, 20))
 
-# # 그리드 라인 그리기
-# for x in range(grid_size_x + 1):
-#     ax.axhline(x, lw=2, color='k', zorder=5)
-# for y in range(grid_size_y + 1):
-#     ax.axvline(y, lw=2, color='k', zorder=5)
+# 그리드 라인 그리기
+for x in range(grid_size_x + 1):
+    ax.axhline(x, lw=2, color='k', zorder=5)
+for y in range(grid_size_y + 1):
+    ax.axvline(y, lw=2, color='k', zorder=5)
 
-# # 그리드 셀 사이즈 조정
-# ax.set_xticks(np.arange(grid_size_x + 1))
-# ax.set_yticks(np.arange(grid_size_y + 1))
-# ax.set_xticklabels([])
-# ax.set_yticklabels([])
-# ax.grid(True)
+# 그리드 셀 사이즈 조정
+ax.set_xticks(np.arange(grid_size_x + 1))
+ax.set_yticks(np.arange(grid_size_y + 1))
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.grid(True)
 
-# # 꼭 지나야 할 점과 가지 말아야 할 점 표시
-# for j in range(len(must_avoid)):
-#     ax.add_patch(plt.Rectangle((must_avoid[j][1], must_avoid[j][0]), 1, 1, color='red', alpha=0.5))
+# 꼭 지나야 할 점과 가지 말아야 할 점 표시
+for j in range(len(must_avoid)):
+    ax.add_patch(plt.Rectangle((must_avoid[j][1], must_avoid[j][0]), 1, 1, color='red', alpha=0.5))
 
-# # 각 셀에 좌표 표시
-# for x in range(grid_size_x):
-#     for y in range(grid_size_y):
-#         ax.text(y + 0.5, x + 0.5, f'({x},{y})', ha='center', va='center', fontsize=0.5)
+# 각 셀에 좌표 표시
+for x in range(grid_size_x):
+    for y in range(grid_size_y):
+        ax.text(y + 0.5, x + 0.5, f'({x},{y})', ha='center', va='center', fontsize=5)
 
-# # 시작점과 도착점 표시
-# ax.text(start_point[1] + 0.1, start_point[0] + 1 - 0.1, 'Start', ha='left', va='bottom', fontweight='bold', fontsize=12, color='blue', zorder=10)
-# ax.text(goal_point[1] + 0.1, goal_point[0] + 1 - 0.1, 'Goal', ha='left', va='bottom', fontweight='bold', fontsize=12, color='red', zorder=10)
+# 시작점과 도착점 표시
+ax.text(start_point[1] + 0.1, start_point[0] + 1 - 0.1, 'Start', ha='left', va='bottom', fontweight='bold', fontsize=12, color='blue', zorder=10)
+ax.text(goal_point[1] + 0.1, goal_point[0] + 1 - 0.1, 'Goal', ha='left', va='bottom', fontweight='bold', fontsize=12, color='red', zorder=10)
 
-# # 최적 경로 그리기
-# for i in range(len(optimal_path) - 1):
-#     start = optimal_path[i]
-#     end = optimal_path[i + 1]
-#     ax.plot([start[1] + 0.5, end[1] + 0.5], [start[0] + 0.5, end[0] + 0.5], 'o-', color='green', lw=1, markersize=3, zorder=10)
+# 최적 경로 그리기
+for i in range(len(optimal_path) - 1):
+    start = optimal_path[i]
+    end = optimal_path[i + 1]
+    ax.plot([start[1] + 0.5, end[1] + 0.5], [start[0] + 0.5, end[0] + 0.5], 'o-', color='green', lw=1, markersize=3, zorder=10)
 
-# plt.gca().invert_yaxis()
-# plt.axis('equal')
-# plt.savefig("grid_plot.svg", format="svg")
-# plt.show()
+plt.gca().invert_yaxis()
+plt.axis('equal')
+plt.savefig("grid_plot.svg", format="svg")
+plt.show()
